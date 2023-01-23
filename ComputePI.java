7b71eb2ec8d05b070d0b5d99c497ed47f6b383b5
Nouwen Mokua <91521950+Nouwen1@users.noreply.github.com>
2023-01-23 09:47:46 +0300

    Add files via upload

---
+++ ComputePI.java
@@ -0,0 +1,24 @@
+public class ComputePI {
+    public static void main(String[]args){
+        double precision = 0.0000001;
+        ComputePI aComputePI = new ComputePI();
+        double pi = aComputePI.getPI(precision);
+        System.out.printf("Difference between the values obtained and the Math.pi: %.10f \n", (double)(Math.PI - pi));
+
+    }
+
+    private double getPI(double precision){
+        double sumOld;
+        double sumNew = 0.0;
+        long i = 0;
+        do{
+            sumOld = sumNew;
+            sumNew += ((i % 2 == 0) ? 1 : -1) * (double) 4/(2*i+1);
+            ++i;
+        }while( (double) Math.abs(sumNew - sumOld) >= precision);
+
+        System.out.printf("Calculated items %d, PI = %.10f", i, sumOld);
+        System.out.println();
+        return sumOld;
+    }
+}
